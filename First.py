28061b4497dcecf1ebf073eeef7bc8ab556451be
MihiranPerera <160620353+MihiranPerera@users.noreply.github.com>
2024-05-21 22:46:46 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,28 @@
+#01
+print("Hello World")
+
+#02
+print("Hello \nWorld")
+
+#03
+print("\nHello \nWorld")
+
+#04
+print("""\nHello
+World
+Hello""")
+
+#05
+print("\nHello",end="")
+print("World")
+
+#06
+print("\nHello" , end=" ")
+print("World")
+
+#07
+print("\nA","B","C","D")
+
+#08
+print("\nA","B","C","D" , sep=",")
+
