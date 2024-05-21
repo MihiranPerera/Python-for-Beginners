30d244f34ee417c877cdbabafc33efd40e51133b
MihiranPerera <160620353+MihiranPerera@users.noreply.github.com>
2024-05-21 23:07:35 +0530

    Add files via upload

---
+++ Variables part 01/Variables_Part_01.py
@@ -0,0 +1,16 @@
+#Variable name
+#01. start from a letter or "_"
+#02. no spaces
+#03. no symbols except "_"
+
+a = 10
+b = 55.2
+weight = 50
+_index = 200
+
+print(a)
+print(b)
+print("\nFirst variable:", a)
+print("Second variable:", b)
+print(weight)
+print(_index)
