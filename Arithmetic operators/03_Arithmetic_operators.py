e45f93d20a23b646375ff0233fc3d1507a6802c8
MihiranPerera <160620353+MihiranPerera@users.noreply.github.com>
2024-05-22 22:48:57 +0530

    Add files via upload

---
+++ Arithmetic operators/03_Arithmetic_operators.py
@@ -0,0 +1,24 @@
+# Addition          - "+"
+# Subtraction       - "-"
+# Division          - "/"
+# Multiplication    - "*"
+# Modulus           - "%"
+# Exponentiation    - "**"
+
+################################################
+# Calculate precidence
+################################################
+# Left to Right
+# "()" --> "**" --> "*" --> "+"
+
+print("Addition:",5+5)
+print("Subtraction:",10-5)
+print("Division:", 10/5)
+print("Multiplication:", 5*2)
+print("Modulus:", 9%2)
+print("Exponentiation:", 2**3)
+
+Num = 10
+calculate = 104 - Num
+
+print("\nAnswer:" , calculate)
